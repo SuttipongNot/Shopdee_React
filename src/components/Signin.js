520da47a40cfdeb6ff538c7fe13c1186d1e2a6de
Alxnee <suttipong.poo@rmutto.ac.th>
2024-09-10 11:39:26 +0700

    version 1.1

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,3 @@
-import React, {useState} from "react";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -13,6 +12,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
+import React, {useState} from "react";
 
 function Copyright(props) {
   return (
@@ -31,29 +31,29 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-export default function SignUp() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+export default function SignIn() {
+ const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+      e.preventDefault();
 
-        const response = await axios.post(process.env.REACT_APP_BASE_URL+'/login',
-            {
-                username, 
-                password
-            }
-        );
+      const response = await axios.post(process.env.REACT_APP_BASE_URL+'/login',
+          {
+              username, 
+              password
+          }
+      );
 
-        const result = response.data;
-        console.log(result);
-        alert(result['message']);
+      const result = response.data;
+      console.log(result);
+      alert(result['message']);
 
-        if(result['status'] === true){
-            localStorage.setItem('token', result['token']);
-            window.location.href = '/';
-        }
-    }
+      if(result['status'] === true){
+          localStorage.setItem('token', result['token']);
+          window.location.href = '/';
+      }
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -71,83 +71,62 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Sign in
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  name="Username"
-                  autoComplete="username"
-                  value={username}
-                  onChange={ (e) => setUsername(e.target.value) }
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={password}
-                  onChange={ (e) => setPassword(e.target.value) }
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="username"
+              name="username"
+              autoComplete="username"
+              value={username}
+                onChange={ (e) => setUsername(e.target.value) }
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={ (e) => setPassword(e.target.value) }
+              autoComplete="current-password"
+
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Sign In
             </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                  {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
+        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
-
 }
